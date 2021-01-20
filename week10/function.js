adae631dd2849e5717513d95a75077c4f87d6214
gulsahatalan <atalangulsah@gmail.com>
2021-01-21 00:41:33 +0100

    add week10

---
+++ week10/function.js
@@ -0,0 +1,64 @@
+function showProductList() {
+  let isim = `<table class="table caption-top">
+    <caption class="captionText" >PRODUCT LIST</caption>
+    <tr>
+    <th>Name</th>
+    <th>Calori</th>
+    <th>Exp. Date</th>
+    <th>Price</th>
+    <th></th>
+    <th></th>
+    </tr>`;
+  productList.map((product) => {
+    isim += `<tr>
+      <td>${product.productName}</td>
+      <td>${product.totalCalories}</td>
+      <td>${product.expireDate.getTime()}</td>
+      <td>${product.price}</td>
+      <td> <img src="${product.productImage}"/></td>
+      <td> <button id="${product.productName}" class="buy">BUY</button></td>
+      <tr>`;
+  });
+  isim += `</table>`;
+  return isim;
+}
+
+function showShoppingList(pList) {
+  let total = 0;
+  let shopList = `<table class="table caption-top">
+    <caption class="captionText" >SHOPPING LIST</caption>
+    <tr>
+    <th>Name</th>
+    <th>Price</th>
+    </tr>
+    `;
+  pList.map((product, index) => {
+    total += product.price;
+    shopList += `<tr>
+      <td>${product.productName}</td>
+      <td>${product.price}</td>
+      <td> <img src="${product.productImage}"/></td>
+      <td> <button id="${index}" class="delete">Delete</button></td>
+      <tr>
+     `;
+  });
+  shopList += `<tr> <td id="toplam">Toplam fiyat: ${total.toFixed(2)}</td></tr>
+   </table>`;
+  return shopList;
+}
+
+function addElement(pList) {
+  productList.map((product) => {
+    if (product.productName === event.target.id) {
+      pList.push(product);
+    }
+  });
+}
+
+function showTotal() {
+  let lastTotal = 0;
+  shoppingList.map((product) => {
+    lastTotal += product.price;
+    return lastTotal;
+  });
+}
